4843c883c4ef05d1fde2eb655d05a3fc283e34d9
Vinh <vinhbuith32412000@gmail.com>
2022-06-11 11:45:19 +0700

    Fix bugs and edit width of container to remain consistent

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const WEATHER_API_KEY = '4acd642e09a418b582720e5119fcab06';
 async function getLocation(cityName="", stateCode="", countryCode=""){
   try {
     const cityInfo = [cityName, stateCode, countryCode].filter(info => info != "").join(",");
-    const response = await fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${cityInfo}&limit=5&appid=${WEATHER_API_KEY}`, {mode: 'cors'});
+    const response = await fetch(`https://api.openweathermap.org/geo/1.0/direct?q=${cityInfo}&limit=5&appid=${WEATHER_API_KEY}`, {mode: 'cors'});
     const location = await response.json();
     return {"lat": location[0].lat, "lon": location[0].lon};
   } catch {
