76f19c1383412f984aeb8e250a2ef143141d513f
Vinh <vinhbuith32412000@gmail.com>
2022-06-11 10:24:30 +0700

    Complete Project

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const WEATHER_API_KEY = '4acd642e09a418b582720e5119fcab06'
+const WEATHER_API_KEY = '4acd642e09a418b582720e5119fcab06';
 
 async function getLocation(cityName="", stateCode="", countryCode=""){
   try {
@@ -21,13 +21,69 @@ async function getWeatherInfo(location) {
   }
 }
 
+function convertKtoC(K) {
+  if(isNaN(K)){
+    console.log("Error! Input must be a number");
+    return;
+  }
+
+  return Math.round(K - 273.15);
+}
+
+function getTimeStr() {
+  let todayTime = new Date();
+  const date = todayTime.getDate();
+  const day = todayTime.toLocaleDateString('en-US', { weekday: 'short' });   
+  const month = new Intl.DateTimeFormat('en-US', {month: 'short'}).format(todayTime);
+  return `${day} ${date} ${month}`;
+}
+
+function capitalizeFirstLetter(string) {
+  return string.charAt(0).toUpperCase() + string.slice(1);
+}
+
+function generalizeInfo(info){
+  const location = info.name;
+  const displayLocation = document.getElementById("location");
+  displayLocation.innerText = `Location: ${location}`;
+
+  const time = getTimeStr();
+  const displayTime = document.getElementById("time");
+  displayTime.innerText = time;
+
+  const temperature = convertKtoC(Number(info.main.temp));
+  const displayTemp = document.getElementById("temp");
+  displayTemp.innerText = `${temperature}°`;
+
+  const feelsLike = convertKtoC(Number(info.main["feels_like"]));
+  const displayFeelsLike = document.getElementById("feels-like");
+  displayFeelsLike.innerText = `Feels like: ${feelsLike}°`;
+
+  const displayDescription = document.getElementById("description");
+  displayDescription.innerText = capitalizeFirstLetter(info.weather[0].description);
+
+  const displayHumidity = document.getElementById("humidity");
+  displayHumidity.innerText = `Humidity: ${info.main.humidity}%`;
+}
+
+const displayInfo = document.getElementById("display-info");
+
+getLocation("Ho Chi Minh").then(location => {
+  getWeatherInfo(location).then(weatherInfo => {
+    console.log(weatherInfo);
+    generalizeInfo(weatherInfo);
+  })
+})
+
 const weatherForm = document.getElementById("weather-form");
 weatherForm.addEventListener('submit', e => {
   e.preventDefault();
   cityName = weatherForm['city-name'].value;
+
   getLocation(cityName).then(location => {
-    getWeatherInfo(location).then(info => {
-      console.log(info);
+    getWeatherInfo(location).then(weatherInfo => {
+      generalizeInfo(weatherInfo);
+      console.log(weatherInfo);
     })
   })
 })
